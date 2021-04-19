efe93c40f2134c273a7988caee05eb0a47e22e4f
DavidMeda <davidemedaglia29@hotmail.it>
2021-04-19 02:11:58 +0200

    aggiustato allert in base al click bottone addMinestry

--- webapp/app.js
+++ webapp/app.js
@@ -5,13 +5,21 @@ console.log("HELLO");
 $('document').ready(function(){
 
 document.getElementById("buttonAddressMinestry").onclick = function() {myFunction()};
+var inputAddressMinestry = document.getElementById("inputAddressMinestry")
 
 function myFunction() {
-  if (document.getElementById("inputAddressMinestry").value == "0x"){
-  	console.log("click");
+  if (inputAddressMinestry.value == "0x"){
+  	inputAddressMinestry.value = '';
+  	$("#allertMinestry").html('<div class="alert alert-success alert-dismissible fade show" id="allertMinestry2" role="alert"><strong>Transaction executed!</strong> blablalbs.  <button type="button" class="close" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">×</span>  </button></div></div>');
+  	// this will automatically close the alert and remove this if the users doesnt close it in 5 sec
+  	setTimeout(function() {$("#allertMinestry2").remove();}, 10000);
   }
   else{
-  	console.log("No click");
+  	inputAddressMinestry.value = '';
+  	$("#allertMinestry").style = '';
+  	$("#allertMinestry").html('<div class="alert alert-danger alert-dismissible fade show" id="allertMinestry2" role="alert"><strong>Transaction NOT executed!</strong> blablalbs.  <button type="button" class="close" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">×</span>  </button></div></div>');
+  	// this will automatically close the alert and remove this if the users doesnt close it in 5 sec
+  	setTimeout(function() {$("#allertMinestry2").remove();}, 10000);
   }
 }
 
